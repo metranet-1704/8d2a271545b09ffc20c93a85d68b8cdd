95f539c3900089c366408b83591659f57567e87a
Metranet <piket.metranet@gmail.com>
2024-07-19 18:37:20 +0700

    e3816975aa98e2d876c9d2824e4274046d0c2717

---
+++ _next/static/chunks/07115393-9ec2cf5650d635a9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[682],{2956:function(t,n,a){a.d(n,{Zh5:function(){return e}});var u=a(8534);function e(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M12 18a6 6 0 1 0 0-12 6 6 0 0 0 0 12Z"}}]})(t)}}}]);
