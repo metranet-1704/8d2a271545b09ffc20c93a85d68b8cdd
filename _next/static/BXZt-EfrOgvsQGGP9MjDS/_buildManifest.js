4e96802d0cfa5f46f65932a4d429fcb9b27df184
Metranet <piket.metranet@gmail.com>
2026-03-25 11:08:38 +0000

    c518795fac29d1d90f1abf8a1cd16f9cfa492236

---
+++ _next/static/BXZt-EfrOgvsQGGP9MjDS/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/_axiom/web-vitals"
+      },
+      {
+        "source": "/_axiom/logs"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
